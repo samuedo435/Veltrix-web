f66569eb247eb1df727f547f4c2069c8f0dcc969
samuedo435 <samuedo435@gmail.com>
2026-06-09 17:26:55 -0500

    feat: implement routing structure with ProtectedRoute for Home, Productos, and Profile components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,67 @@
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Navigate
+} from "react-router-dom";
+
 import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Productos from "./pages/Productos";
+import Profile from "./pages/Profile";
+
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 function App() {
 
     return (
-        <Login />
-    );
 
+        <BrowserRouter>
+
+            <Routes>
+
+                <Route
+                    path="/login"
+                    element={<Login />}
+                />
+
+                <Route
+                    path="/"
+                    element={
+                        <ProtectedRoute>
+                            <Home />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route
+                    path="/productos"
+                    element={
+                        <ProtectedRoute>
+                            <Productos />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route
+                    path="/profile"
+                    element={
+                        <ProtectedRoute>
+                            <Profile />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route
+                    path="*"
+                    element={<Navigate to="/" />}
+                />
+
+            </Routes>
+
+        </BrowserRouter>
+
+    );
 }
 
 export default App;
