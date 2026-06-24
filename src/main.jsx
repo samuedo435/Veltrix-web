1e8b9dcd7a649208837176d266a7bf5b1f139c64
samuedo435 <samuedo435@gmail.com>
2026-06-24 11:13:28 -0500

    feat: add Bootstrap integration and create new styles for Footer, Navbar, and ProductCard components

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
+import "./styles/theme.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(
     document.getElementById("root")
